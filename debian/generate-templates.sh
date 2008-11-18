d8f9cee5ccf3e93273406100398bb5b79806baa1
An-Cheng Huang <ancheng@build-vm.localdomain>
2008-11-18 13:09:52 -0800

    Revert "rename packages to vyatta-snort*"
    This reverts commit 12410fab9c6e83281a019e8986bbc5527748ba97.

--- debian/generate-templates.sh
+++ debian/generate-templates.sh
@@ -6,17 +6,17 @@
 for package in ""; do
     packagename=$package
     [ -n "$package" ] && packagename="-$packagename"
-    OUTPUT="vyatta-snort$packagename.templates"
-    echo "Generating templates for vyatta-snort$packagename at $OUTPUT"
-    cat vyatta-snort.TEMPLATE.templates | sed -e "s/{PACKAGE}/$packagename/g" >$OUTPUT
+    OUTPUT="snort$packagename.templates"
+    echo "Generating templates for snort$packagename at $OUTPUT"
+    cat snort.TEMPLATE.templates | sed -e "s/{PACKAGE}/$packagename/g" >$OUTPUT
     # Add Database templates also
     if [ "$package" = "mysql" ] || [ "$package" = "pgsql" ] ; then
-        cat vyatta-snort.DATABASE.templates | sed -e "s/{PACKAGE}/$packagename/g" | sed -e "s/{DATABASE}/$package/g" >>$OUTPUT
+        cat snort.DATABASE.templates | sed -e "s/{PACKAGE}/$packagename/g" | sed -e "s/{DATABASE}/$package/g" >>$OUTPUT
     fi
             
     # Finally, add any additional templates this package might have
-    if [ -e "vyatta-snort$packagename.ADD.templates" ] ; then
-        cat "vyatta-snort$packagename.ADD.templates"  >>$OUTPUT
+    if [ -e "snort$packagename.ADD.templates" ] ; then
+        cat "snort$packagename.ADD.templates"  >>$OUTPUT
     fi
 done
 
